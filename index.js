02417330c3593fcc4390a51584f155c05191b446
Lei <leilachiderski@gmail.com>
2026-03-12 08:34:52 -0300

    UDP message sending structure

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 "use strict";
 const fs = require('fs');
 const bencode = require('bencode.js');
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
 
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-console.log(torrent.announce.toString('utf8'))
+const url = urlParse(torrent.announce.toString('utf8'));
+const socket = dgram.createSocket('udp4');
+const myMsg = Buffer.from('Hello!', 'utf8');
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+socket.on('message', msg => {
+    console.log('The message is ' + msg);
+});
