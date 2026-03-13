f437b5750219e3f98d77ab3846d93f592df0abb9
Lei <leilachiderski@gmail.com>
2026-03-13 20:36:19 -0300

    UDP tracker protocol

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 "use strict";
 const fs = require('fs');
 const bencode = require('bencode.js');
-const dgram = require('dgram');
-const Buffer = require('buffer').Buffer;
-const urlParse = require('url').parse;
+const tracker = require('./tracker');
 
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-const url = urlParse(torrent.announce.toString('utf8'));
-const socket = dgram.createSocket('udp4');
-const myMsg = Buffer.from('Hello!', 'utf8');
 
-socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
-socket.on('message', msg => {
-    console.log('The message is ' + msg);
+tracker.getPeers(torrent, peers => {
+    console.log('List of peers: ', peers);
 });
