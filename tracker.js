2981847d9b3520889c5d5b15a6a3a4343afb6a19
Lei <leilachiderski@gmail.com>
2026-03-16 19:39:36 -0300

    Announce request

--- tracker.js
+++ tracker.js
@@ -3,6 +3,8 @@
 const dgram = require('dgram');
 const Buffer = require('buffer').Buffer;
 const urlParse = require('url').parse;
+const torrentParser = require('./torrent-parser');
+const util = require('./util');
 
 module.exports.getPeers = (torrent, callback) => {
     const socket = dgram.createSocket('udp4');
@@ -21,6 +23,8 @@ module.exports.getPeers = (torrent, callback) => {
             callback(announceResp.peers);
         }
     });
+
+    const announceReq = buildAnnounceReq(connResp.connId, torrent);
 }
 
 const udpSend = (socket, message, rawUrl, callback = () => {}) => {
@@ -46,12 +50,26 @@ const parseConnResp = (resp) => {
     return{
         action: resp.readUint32BE(0),
         transactionId: resp.readUint32BE(4),
-        connectionId: resp.readUint32BE(8)
+        connectionId: resp.slice(8)
     }
 }
 
-const buildAnnounceReq = (connId) => {
-
+const buildAnnounceReq = (connId, torrent, port=6881) => {
+    const buf = Buffer.allocUnsafe(98);
+    connId.copy(buf, 0);
+    buf.writeUInt32BE(1, 8);
+    crypto.randomBytes(4).copy(buf, 12);
+    torrentParser.infoHash(torrent).buf(16);
+    util.genId().copy(buf, 36);
+    Buffer.alloc(8).copy(buf, 56);
+    torrentParser.size(torrent).copy(buf, 64);
+    Buffer.alloc(8).copy(buf, 72);
+    buf.writeUInt32BE(0, 80);
+    buf.writeUint32BE(0, 80);
+    crypto.randomBytes(4).copy(buf, 88);
+    buf.writeInt32BE(-1, 92);
+    buf.writeUInt16BE(port, 96);
+    return buf;
 }
 
 const parseAnnounceResp = (resp) => {
